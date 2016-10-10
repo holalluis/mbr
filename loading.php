c24e5065f7bbcd6f419d07244348892a7f73bfc9
unknown <lbosch@lboschpc.ICRA.LOCAL>
2016-10-10 20:00:41 +0200

    prova des de commander

--- loading.php
+++ loading.php
@@ -1,12 +1,12 @@
 Loading 
 <span style="
-		height:100px;
-		width:100px;
-		font-size:30px;
-		animation-name:spin;
-		animation-duration: 2000ms;
-		animation-iteration-count: infinite;
-		animation-timing-function: linear;
+	height:100px;
+	width:100px;
+	font-size:30px;
+	animation-name:spin;
+	animation-duration: 2000ms;
+	animation-iteration-count: infinite;
+	animation-timing-function: linear;
 ">
 &#128259 
 </span> 

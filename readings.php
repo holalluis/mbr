8befc78b14955e82c810f7d528d261d2eacb1d0f
lbosch <lbosch@icra.cat>
2017-03-17 20:04:04 +0100

    sql readings

--- readings.php
+++ readings.php
@@ -17,15 +17,6 @@
 
 <!--TITLE AND QUERY-->
 <?php
-	$sql="
-		SELECT * 
-		FROM (SELECT * FROM readings ORDER BY date DESC) AS r,devices 
-		WHERE devices.id=r.id_device
-		GROUP BY r.id_device
-	";
-	$res=mysql_query($sql) or die(mysql_error());
-	$dbSize=current(mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM readings")));
-	$results=mysql_num_rows($res);
 ?>
 
 <!--READINGS-->
@@ -35,34 +26,43 @@
 		<th>Value
 		<th>Unit
 		<th>When
-	<?php
-		while ($row=mysql_fetch_assoc($res))
-		{
-			$id_device		= $row['id_device'];
-			$date			= $row['date'];
-			$value			= $row['value'];
-			$name			= $row['name'];
-			$plcPosition	= $row['plcPosition'];
-			$type			= $row['type'];
-			$unit			= $row['unit'];
-			$ubication		= $row['ubication'];
-			//time ago
-			$ago=timeAgo($date);
-			//display
-			$value=round($value,4);
-			if($unit==""){$unit="<span style=color:#ccc>no unit</span>";}
-			echo "<tr>
-				<td><a href=device.php?id=$id_device title='$type, $ubication'>$name</a>
-				<td>$value
-				<td>$unit
-				<td title='$date'>$ago
+		<?php
+			$sql="
+				SELECT * 
+				FROM devices, (SELECT * FROM readings ORDER BY readings.date DESC) as r
+				WHERE devices.id=r.id_device
+				GROUP BY r.id_device
 			";
-		}
-		if(mysql_num_rows($res)==0)
-		{
-			echo "<tr><td colspan=7>~No readings inserted yet";
-		}
-	?>
+			$res=mysql_query($sql) or die(mysql_error());
+			while($row=mysql_fetch_assoc($res))
+			{
+				$id_device		= $row['id_device'];
+				$date			= $row['date'];
+				$value			= $row['value'];
+				$name			= $row['name'];
+				$plcPosition	= $row['plcPosition'];
+				$type			= $row['type'];
+				$unit			= $row['unit'];
+				$ubication		= $row['ubication'];
+				//time ago
+				$ago=timeAgo($date);
+				//display
+				$value=round($value,4);
+				if($unit==""){$unit="<span style=color:#ccc>no unit</span>";}
+				echo "<tr>
+					<td><a href=device.php?id=$id_device title='$type, $ubication'>$name</a>
+					<td>$value
+					<td>$unit
+					<td title='$date'>$ago
+				";
+			}
+			if(mysql_num_rows($res)==0)
+			{
+				echo "<tr><td colspan=7>~No readings inserted yet";
+			}
+			$dbSize=current(mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM readings")));
+			$results=mysql_num_rows($res);
+		?>
 	<tr><th colspan=7>[...<?php echo ($dbSize-$results)." more"?>...]
 </table>
 

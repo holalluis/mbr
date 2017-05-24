0c4cf666eae9034b49cc2ad7a805c4d41cd50e4f
lbosch <lbosch@icra.cat>
2017-05-24 18:55:03 +0100

    timer

--- supervisor/timer.py
+++ supervisor/timer.py
@@ -1,6 +1,7 @@
 ''' 
 	Use supervisor library to Read PLC each 10 seconds and insert values to mysql DB
 '''
+import os
 import time
 import supervisor
 
@@ -14,15 +15,28 @@ print "+------------------------+"
 print "| Recording PLC to MySQL | Sample Time: %s seconds" % sampleTime
 print "+------------------------+"
 
+if sys.platform.find('win')==0:
+    clear='cls'
+else:
+    clear='clear'
+
+i=0;
 while True:
-	opcState = opc.ping() #check connection
-	if not(opcState): print ' [!] ERROR not connected to PLC. Exiting...'; break;
 
-	print "	=== New Reading: %s" % (time.asctime()) 	#display time 
+    opcState = opc.ping() #check connection
+    if not(opcState): print ' [!] ERROR not connected to PLC. Exiting...'; break;
 
-	#iterate device types that we want to store
-	deviceTypes=['Sensor','Alarm','Equipment','Setpoint']
-	supervisor.readAndStore(cursor,opc,deviceTypes)
+    print "	=== New Reading: %s" % (time.asctime()) 	#display time 
 
-	print "	[+] Sleeping %s seconds...\n\n" % (sampleTime)
-	time.sleep(sampleTime)
+    #iterate device types that we want to store
+    deviceTypes=['Sensor','Alarm','Equipment','Setpoint']
+    supervisor.readAndStore(cursor,opc,deviceTypes)
+
+    print "	[+] Sleeping %s seconds...\n\n" % (sampleTime)
+    time.sleep(sampleTime)
+
+    i=i+1;
+
+    if i==10: 
+        os.system(clear); 
+        i=0;

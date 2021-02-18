2df6a5b815989af107bdfe99eabffbdc0bc795ab
MarxDaMan <63607101+MarxDaMan@users.noreply.github.com>
2021-02-18 14:06:40 +0100

    python 3

--- supervisor/timer.py
+++ supervisor/timer.py
@@ -1,43 +1,46 @@
-'''
+"""
   Use supervisor library to Read PLC each 10 seconds and insert values to mysql DB
-'''
+"""
 import os
 import time
 import supervisor
 import sys
 
-#set sample time (seconds)
-sampleTime=10
+# set sample time (seconds)
+sampleTime = 10
 
-#connect to plc and mysql
-[opc,cursor]=supervisor.connect()
+# connect to plc and mysql
+[opc,cursor] = supervisor.connect()
 
-print ("+------------------------+")
-print (f"| Recording PLC to MySQL | Sample Time: {sampleTime} seconds")
-print ("+------------------------+")
+print("+------------------------+")
+print(f"| Recording PLC to MySQL | Sample Time: {sampleTime} seconds")
+print("+------------------------+")
 
-if sys.platform.find('win')==0:
-    clear='cls'
+if sys.platform.find('win') == 0:
+    clear = 'cls'
 else:
-    clear='clear'
+    clear = 'clear'
 
-i=0;
+i = 0
 while True:
 
-    opcState = opc.ping() #check connection
-    if not(opcState): print ' [!] ERROR not connected to PLC. Exiting...'; break;
+    opcState = opc.ping()  # check connection
+    if not opcState:
+        print(' [!] ERROR not connected to PLC. Exiting...')
 
-    print (" === New Reading: %s" % (time.asctime()))  #display time
+        break
 
-    #iterate device types that we want to store
-    deviceTypes=['Sensor','Alarm','Equipment','Setpoint']
+    print(" === New Reading: %s" % (time.asctime()))  # display time
+
+    # iterate device types that we want to store
+    deviceTypes = ['Sensor','Alarm','Equipment','Setpoint']
     supervisor.readAndStore(cursor,opc,deviceTypes)
 
-    print (" [+] Sleeping %s seconds...\n\n" % (sampleTime))
+    print(" [+] Sleeping %s seconds...\n\n" % (sampleTime))
     time.sleep(sampleTime)
 
-    i=i+1;
+    i = i+1
 
-    if i==5:
-        os.system(clear);
-        i=0;
+    if i == 5:
+        os.system(clear)
+        i = 0
